d36c0a4fc64bc6de1b7dd5c561a146ed40ee776f
vishesh0123 <visheshsachdev04@gmail.com>
2022-12-26 19:47:49 +0530

    prettier update

--- contracts/VIP Bank/VIP_Bank.sol
+++ contracts/VIP Bank/VIP_Bank.sol
@@ -27,18 +27,12 @@ contract VIP_Bank {
     }
 
     function deposit() public payable onlyVIP {
-        require(
-            msg.value <= 0.05 ether,
-            "Cannot deposit more than 0.05 ETH per transaction"
-        );
+        require(msg.value <= 0.05 ether, "Cannot deposit more than 0.05 ETH per transaction");
         balances[msg.sender] += msg.value;
     }
 
     function withdraw(uint256 _amount) public onlyVIP {
-        require(
-            address(this).balance <= maxETH,
-            "Cannot withdraw more than 0.5 ETH per transaction"
-        );
+        require(address(this).balance <= maxETH, "Cannot withdraw more than 0.5 ETH per transaction");
         require(balances[msg.sender] >= _amount, "Not enough ether");
         balances[msg.sender] -= _amount;
         (bool success, ) = payable(msg.sender).call{value: _amount}("");
---
+++ contracts/safeNFT/safeNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.7;
+
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract safeNFT is ERC721Enumerable {
+    uint256 price;
+    mapping(address => bool) public canClaim;
+
+    constructor(string memory tokenName, string memory tokenSymbol, uint256 _price) ERC721(tokenName, tokenSymbol) {
+        price = _price; //price = 0.01 ETH
+    }
+
+    function buyNFT() external payable {
+        require(price == msg.value, "INVALID_VALUE");
+        canClaim[msg.sender] = true;
+    }
+
+    function claim() external {
+        require(canClaim[msg.sender], "CANT_MINT");
+        _safeMint(msg.sender, totalSupply());
+        canClaim[msg.sender] = false;
+    }
+}
